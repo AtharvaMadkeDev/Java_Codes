68e4dced4f2530bfff8890a093bfd1e545dafa95
AtharvaMadkeDev <atharvamadke22@gmail.com>
2025-10-28 15:22:38 +0530

    Java Codes

--- Java_New_For_Githubrepo/src_logicalprograms/Local_variable_logical_programs/Sum_of_first_ten_natural_numbers.java
+++ Java_New_For_Githubrepo/src_logicalprograms/Local_variable_logical_programs/Sum_of_first_ten_natural_numbers.java
@@ -3,8 +3,24 @@ package Local_variable_logical_programs;
 public class Sum_of_first_ten_natural_numbers {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Natural_nos n = new Natural_nos();
+		n.display();
 
 	}
 
+}
+
+class Natural_nos
+{
+	public void display()
+	{
+		int sum=0;
+		for(int i=1;i<=10;i++)
+		{
+			sum=sum+i;
+		}
+		
+		System.out.println("Sum : "+sum);
+	}
 }
