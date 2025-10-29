881355e0d24ea99d8fa71d94459c86f9d8e0a50b
AtharvaMadkeDev <atharvamadke22@gmail.com>
2025-10-29 13:01:58 +0530

    Java codes

---
+++ Java_New_For_Githubrepo/src_logicalprograms/Static_variable_logical_programs/Count_number_of_objects_created.java
@@ -0,0 +1,41 @@
+package Static_variable_logical_programs;
+
+public class Count_number_of_objects_created {
+
+	public static void main(String[] args) {
+		
+		objectCounter obj1 = new objectCounter();
+		obj1.display();
+		System.out.println();
+		
+		objectCounter obj2 = new objectCounter();
+		obj2.display();
+		System.out.println();
+		
+		objectCounter obj3 = new objectCounter();
+		obj3.display();
+		System.out.println();
+		
+		
+		objectCounter newdisp = new objectCounter();
+		System.out.println("Total objects created : "+newdisp.counter);
+	}
+
+}
+
+class objectCounter
+{
+	static int counter=0;
+	
+	public void display()
+	{
+		counter++;
+		System.out.println("Object "+counter+" created!");
+		
+	}
+	
+	public void display2()
+	{
+		System.out.println(counter);
+	}
+}
