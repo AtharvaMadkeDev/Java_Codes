298f68f74214e70e8d89bb8a0d042ccd0bda1225
AtharvaMadkeDev <atharvamadke22@gmail.com>
2025-10-30 16:25:16 +0530

    Java Codes

---
+++ Java_New_For_Githubrepo/src_logicalprograms/Typecasting_logical_programs/Narrowing_2.java
@@ -0,0 +1,13 @@
+package Typecasting_logical_programs;
+
+public class Narrowing_2 {
+
+	public static void main(String[] args) {
+		
+		long milliseconds = 123456789L;
+		int ms = (int) milliseconds;
+		System.out.println(milliseconds);
+		System.out.println(ms);
+	}
+
+}
