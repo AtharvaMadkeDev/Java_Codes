4abfc93493cfd09bc13c41104e3c7ddad8e4c2e1
AtharvaMadkeDev <atharvamadke22@gmail.com>
2025-10-30 16:08:38 +0530

    Java Codes

---
+++ Java_New_For_Githubrepo/src_logicalprograms/Typecasting_logical_programs/Widening_2.java
@@ -0,0 +1,15 @@
+package Typecasting_logical_programs;
+
+public class Widening_2 {
+
+	public static void main(String[] args) {
+		
+		byte age = 12;
+		int age1 = age;
+		long age2 = age;
+		
+		System.out.println("Values = "+age+" "+age1+" "+age2);
+
+	}
+
+}
