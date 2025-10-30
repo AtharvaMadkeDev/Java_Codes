c932065b5e1d0ce32e38a82cb2907a101a487d4f
AtharvaMadkeDev <atharvamadke22@gmail.com>
2025-10-30 15:46:08 +0530

    java codes

---
+++ Java_New_For_Githubrepo/src/Typecasting/Widening_or_Implicit_casting.java
@@ -0,0 +1,12 @@
+package Typecasting;
+
+public class Widening_or_Implicit_casting {
+
+	public static void main(String[] args) {
+		
+		int num = 10;			
+		double d = num;			//integer to double conversion(Java does it automatically)
+		System.out.println(d);		//Output => 10.0
+	}
+
+}
