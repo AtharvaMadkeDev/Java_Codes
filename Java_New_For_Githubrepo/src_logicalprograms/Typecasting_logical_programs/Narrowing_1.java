cb08b51fd2db766e3a9bc61bcaea92649be692f4
AtharvaMadkeDev <atharvamadke22@gmail.com>
2025-10-30 16:13:21 +0530

    Java codes

---
+++ Java_New_For_Githubrepo/src_logicalprograms/Typecasting_logical_programs/Narrowing_1.java
@@ -0,0 +1,12 @@
+package Typecasting_logical_programs;
+
+public class Narrowing_1 {
+
+	public static void main(String[] args) {
+		
+		double marks = 89.52;
+		int d = (int) marks;
+		System.out.println(d);
+	}
+
+}
