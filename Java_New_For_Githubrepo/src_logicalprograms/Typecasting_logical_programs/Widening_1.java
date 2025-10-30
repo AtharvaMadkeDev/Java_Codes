d6380bed642070c71d6f6b39349a74e181296d56
AtharvaMadkeDev <atharvamadke22@gmail.com>
2025-10-30 16:00:48 +0530

    Java Codes

---
+++ Java_New_For_Githubrepo/src_logicalprograms/Typecasting_logical_programs/Widening_1.java
@@ -0,0 +1,15 @@
+package Typecasting_logical_programs;
+
+public class Widening_1 {
+
+	public static void main(String[] args) {
+		
+		int apples = 5;
+		double weight = 0.15;
+		double d = apples;
+		
+		System.out.println("Total weight = "+weight*d);
+
+	}
+
+}
