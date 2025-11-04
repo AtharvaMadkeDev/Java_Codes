421bc9c397e78f4cba71f178e0dbc7b0537be260
AtharvaMadkeDev <atharvamadke22@gmail.com>
2025-11-04 14:00:13 +0530

    Java Codes

--- Java_New_For_Githubrepo/src_logicalprograms/Array_logical_programs/Sum_of_columns_of_matrix.java
+++ Java_New_For_Githubrepo/src_logicalprograms/Array_logical_programs/Sum_of_columns_of_matrix.java
@@ -51,6 +51,21 @@ public class Sum_of_columns_of_matrix {
 			System.out.println("Sum of "+i+" column = "+sum);
 			sum = 0;
 		}
+		
+		int sum2=0;
+		System.out.println();
+		
+		for(int i=0; i<row; i++)
+		{
+			for(int j=0; j<column; j++)
+			{
+				sum2 = sum2+arr[i][j];
+			}
+			
+			System.out.println();
+			System.out.println("Sum of "+i+" row = "+sum2);
+			sum2=0;
+		}
 	}
 
 }
