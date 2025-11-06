5eaf7c1615c6b15c7c0c444d73b062ff6648f216
AtharvaMadkeDev <atharvamadke22@gmail.com>
2025-11-06 14:01:27 +0530

    Java Codes

---
+++ Java_New_For_Githubrepo/src_logicalprograms/Array_logical_programs/Different_elements_in_different_rows.java
@@ -0,0 +1,38 @@
+package Array_logical_programs;
+import java.util.Scanner;
+public class Different_elements_in_different_rows {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		int arr[][] = new int [3][];
+		
+		arr[0] = new int [3];
+		arr[1] = new int [5];
+		arr[2] = new int [2];
+		
+		for(int i=0; i<arr.length; i++)
+		{
+			for(int j=0; j<arr[i].length; j++)
+			{
+				System.out.println("Enter the "+i+", "+j+" th element :");
+				arr[i][j] = scan.nextInt();
+			}
+			System.out.println();
+		}
+		
+		System.out.println("The elements u have entered are :");
+		
+		for(int i=0; i<arr.length; i++)
+		{
+			for(int j=0; j<arr[i].length; j++)
+			{
+				
+				System.out.print(arr[i][j]+" ");
+			}
+			System.out.println();
+		}
+	}
+
+}
