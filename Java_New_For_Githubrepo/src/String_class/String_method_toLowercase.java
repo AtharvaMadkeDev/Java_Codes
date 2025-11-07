6c273f2f7d48f6ab08f0c2fdddfe2da808762392
AtharvaMadkeDev <atharvamadke22@gmail.com>
2025-11-07 14:21:16 +0530

    Java Codes

---
+++ Java_New_For_Githubrepo/src/String_class/String_method_toLowercase.java
@@ -0,0 +1,14 @@
+package String_class;
+
+public class String_method_toLowercase {
+
+	public static void main(String[] args) {
+		
+		// .toLowercase() => Converts all uppercase values to lowercase
+		
+		String str = "AThaRvA";
+		System.out.println(str.toLowerCase());
+
+	}
+
+}
