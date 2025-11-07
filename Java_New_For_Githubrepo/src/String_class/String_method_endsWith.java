3d7de2096dde9c4cb9e3c5bfb5c9f372da087b1f
AtharvaMadkeDev <atharvamadke22@gmail.com>
2025-11-07 14:51:26 +0530

    Java codes

---
+++ Java_New_For_Githubrepo/src/String_class/String_method_endsWith.java
@@ -0,0 +1,14 @@
+package String_class;
+
+public class String_method_endsWith {
+
+	public static void main(String[] args) {
+		
+		// .endsWith() => Checks whether the string ends with the given value
+		
+		String str = "Atharva";
+		System.out.println(str.endsWith("va"));
+
+	}
+
+}
