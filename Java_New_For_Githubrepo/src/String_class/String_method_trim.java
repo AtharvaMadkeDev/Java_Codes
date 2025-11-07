04a3dc0eef19877f78530a43c25fccd27e7a1c28
AtharvaMadkeDev <atharvamadke22@gmail.com>
2025-11-07 14:46:18 +0530

    Java Codes

---
+++ Java_New_For_Githubrepo/src/String_class/String_method_trim.java
@@ -0,0 +1,14 @@
+package String_class;
+
+public class String_method_trim {
+
+	public static void main(String[] args) {
+		
+		// .trim() => It removes the spaces from the start and well as from the end of the string
+		
+		String str = "    Atharva     ";
+		System.out.println(str.trim());
+
+	}
+
+}
