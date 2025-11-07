a8d5732d1964f0cc09c83d28b35fc6e76f9fd907
AtharvaMadkeDev <atharvamadke22@gmail.com>
2025-11-07 14:25:55 +0530

    Java Codes

---
+++ Java_New_For_Githubrepo/src/String_class/String_method_isBlank.java
@@ -0,0 +1,14 @@
+package String_class;
+
+public class String_method_isBlank {
+
+	public static void main(String[] args) {
+		
+		// .isBlank() => It checks whether the string is blank or not. It counts the blank spaces as well and considers them as blank. 
+		
+		String str = "   ";
+		System.out.println(str.isBlank());
+
+	}
+
+}
