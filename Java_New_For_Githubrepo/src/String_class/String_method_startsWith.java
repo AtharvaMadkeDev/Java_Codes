8ecf8647c0bd222bb696b8a1dab5156159579496
AtharvaMadkeDev <atharvamadke22@gmail.com>
2025-11-07 14:48:44 +0530

    Java Codes

---
+++ Java_New_For_Githubrepo/src/String_class/String_method_startsWith.java
@@ -0,0 +1,15 @@
+package String_class;
+
+public class String_method_startsWith {
+
+	public static void main(String[] args) {
+		
+		// .startsWith => Checks whether the string begins with the given value
+		
+		String str = "Atharva";
+		System.out.println(str.startsWith("At"));
+		
+
+	}
+
+}
