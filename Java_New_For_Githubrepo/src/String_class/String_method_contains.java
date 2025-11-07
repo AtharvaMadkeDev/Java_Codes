a9ad00f563f863d9a143f8ce21c55eb326209ded
AtharvaMadkeDev <atharvamadke22@gmail.com>
2025-11-07 14:38:48 +0530

    Java Codes

---
+++ Java_New_For_Githubrepo/src/String_class/String_method_contains.java
@@ -0,0 +1,16 @@
+package String_class;
+
+public class String_method_contains {
+
+	public static void main(String[] args) {
+		
+		// .contains => It shows whether the value is present in the given string
+		
+		String str = "atharva";
+		System.out.println(str.contains("ath"));
+		
+		
+
+	}
+
+}
