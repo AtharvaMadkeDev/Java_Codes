7d536381b561317a45fa69315ec1051788769142
AtharvaMadkeDev <atharvamadke22@gmail.com>
2025-11-07 14:31:01 +0530

    Java Codes

---
+++ Java_New_For_Githubrepo/src/String_class/String_method_concat.java
@@ -0,0 +1,15 @@
+package String_class;
+
+public class String_method_concat {
+
+	public static void main(String[] args) {
+		
+		// .concat() => It is used to concat(add) the string at the end of the another string
+		
+		String str1 = "Atharva";
+		String str2 = str1.concat(" Madke");
+		
+		System.out.println(str2);
+	}
+
+}
