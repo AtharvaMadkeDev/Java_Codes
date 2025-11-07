21b320c99258fb554531ddd45f7f91f9b7a84c4c
AtharvaMadkeDev <atharvamadke22@gmail.com>
2025-11-07 14:06:28 +0530

    Java codes

---
+++ Java_New_For_Githubrepo/src/String_class/String_method_length.java
@@ -0,0 +1,11 @@
+package String_class;
+
+public class String_method_length {
+
+	public static void main(String[] args) {
+		
+		
+
+	}
+
+}
---
+++ Java_New_For_Githubrepo/src/String_class/What_is_string.java
@@ -0,0 +1,14 @@
+package String_class;
+
+public class What_is_string {
+
+	public static void main(String[] args) {
+		
+		//String is a sequence of characters 
+		
+		String str = "Atharva";
+		System.out.println(str);
+		
+	}
+
+}
