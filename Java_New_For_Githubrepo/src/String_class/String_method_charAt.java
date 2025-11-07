fdc070e1d1e322a83000dd7ed1078173c2b2039f
AtharvaMadkeDev <atharvamadke22@gmail.com>
2025-11-07 14:10:52 +0530

    Java Codes

---
+++ Java_New_For_Githubrepo/src/String_class/String_method_charAt.java
@@ -0,0 +1,15 @@
+package String_class;
+
+public class String_method_charAt {
+
+	public static void main(String[] args) {
+		
+		// .chatAt() => Shows the character present in the given String. Takes index as a input
+		
+		String str = "Atharva";
+		
+		System.out.println(str.charAt(3));
+
+	}
+
+}
