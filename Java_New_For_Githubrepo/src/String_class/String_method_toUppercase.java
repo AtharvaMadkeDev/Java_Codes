285eb483174426dd15e23550796774bb58335532
AtharvaMadkeDev <atharvamadke22@gmail.com>
2025-11-07 14:13:43 +0530

    Java Codes

---
+++ Java_New_For_Githubrepo/src/String_class/String_method_toUppercase.java
@@ -0,0 +1,14 @@
+package String_class;
+
+public class String_method_toUppercase {
+
+	public static void main(String[] args) {
+		
+		// .toUppercase() => Convevrts all the lowercase values into uppercase
+		
+		String str = "Atharva";
+		System.out.println(str.toUpperCase());
+
+	}
+
+}
