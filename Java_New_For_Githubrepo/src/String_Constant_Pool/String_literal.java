42df0dc21dd9f0bcc01c437ac74d347a6b646fc0
AtharvaMadkeDev <atharvamadke22@gmail.com>
2025-11-13 10:57:23 +0530

    Java Codes

---
+++ Java_New_For_Githubrepo/src/String_Constant_Pool/String_literal.java
@@ -0,0 +1,13 @@
+package String_Constant_Pool;
+
+public class String_literal {
+
+	public static void main(String[] args) {
+		
+		//Declaring or defining a String without using the 'new' keyword is called String Literal
+		
+		String name = "Atharva";
+
+	}
+
+}
