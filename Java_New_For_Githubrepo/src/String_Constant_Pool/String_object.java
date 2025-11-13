87bffc38940e2b0e82cf4b9b34559d9b97f0b475
AtharvaMadkeDev <atharvamadke22@gmail.com>
2025-11-13 10:53:22 +0530

    Java Codes

---
+++ Java_New_For_Githubrepo/src/String_Constant_Pool/String_object.java
@@ -0,0 +1,13 @@
+package String_Constant_Pool;
+
+public class String_object {
+
+	public static void main(String[] args) {
+		
+		//Defining or declaring a string using the 'new' keyword is called String Object
+		
+		String name = new String("Atharva");		
+		
+	}
+
+}
