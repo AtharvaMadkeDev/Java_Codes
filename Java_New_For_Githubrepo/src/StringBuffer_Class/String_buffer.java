b5f460ddfc8ebacbce3e29bc5cf3a4b4ed5d4190
AtharvaMadkeDev <atharvamadke22@gmail.com>
2025-11-17 00:52:56 +0530

    Java Codes

---
+++ Java_New_For_Githubrepo/src/StringBuffer_Class/String_buffer.java
@@ -0,0 +1,17 @@
+package StringBuffer_Class;
+
+public class String_buffer {
+
+	public static void main(String[] args) {
+		
+		//StringBuffer is a mutable class which works with the string
+		
+		//Syntax :
+		
+		StringBuffer sb = new StringBuffer("Atharva");
+		
+		System.out.println(sb);
+
+	}
+
+}
