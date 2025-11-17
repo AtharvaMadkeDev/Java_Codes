2c31d064f7068a917c1238aa8be1bb6271ed9b6a
AtharvaMadkeDev <atharvamadke22@gmail.com>
2025-11-17 15:51:23 +0530

    Java Codes

---
+++ Java_New_For_Githubrepo/src/Methods_in_StringBuffer_and_StringBuilder/Method_capacity.java
@@ -0,0 +1,16 @@
+package Methods_in_StringBuffer_and_StringBuilder;
+
+public class Method_capacity {
+
+	public static void main(String[] args) {
+		
+		//Returns the current capacity of the object
+		
+		StringBuffer sb = new StringBuffer();
+		
+		System.out.println(sb.capacity());
+		//default capacity is 16
+		
+	}
+
+}
