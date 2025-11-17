c36aef51d8f08589636ce85ca5bac0e2a7f92a6f
AtharvaMadkeDev <atharvamadke22@gmail.com>
2025-11-17 15:38:37 +0530

    Java Codes

---
+++ Java_New_For_Githubrepo/src/Methods_in_StringBuffer_and_StringBuilder/Method_append.java
@@ -0,0 +1,15 @@
+package Methods_in_StringBuffer_and_StringBuilder;
+
+public class Method_append {
+
+	public static void main(String[] args) {
+		
+		//Adds data at the end of the String
+		
+		StringBuffer sb = new StringBuffer("Hello");
+		sb.append(" World");
+		System.out.println(sb);
+		
+	}
+
+}
