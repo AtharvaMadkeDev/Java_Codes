3c99490b5b82260542639246561ca69b639ddb23
AtharvaMadkeDev <atharvamadke22@gmail.com>
2025-11-17 15:59:54 +0530

    Java Codes

---
+++ Java_New_For_Githubrepo/src/Methods_in_StringBuffer_and_StringBuilder/Method_charAt.java
@@ -0,0 +1,14 @@
+package Methods_in_StringBuffer_and_StringBuilder;
+
+public class Method_charAt {
+
+	public static void main(String[] args) {
+		
+		//It returns the character of the given index
+		
+		StringBuffer sb = new StringBuffer("Atharva");
+		System.out.println(sb.charAt(2));
+
+	}
+
+}
