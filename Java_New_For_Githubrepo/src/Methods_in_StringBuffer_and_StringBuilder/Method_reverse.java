e30dc647eb192b707dde6c9bdb7c24b15db5dcd4
AtharvaMadkeDev <atharvamadke22@gmail.com>
2025-11-17 15:46:59 +0530

    Java Codes

---
+++ Java_New_For_Githubrepo/src/Methods_in_StringBuffer_and_StringBuilder/Method_reverse.java
@@ -0,0 +1,15 @@
+package Methods_in_StringBuffer_and_StringBuilder;
+
+public class Method_reverse {
+
+	public static void main(String[] args) {
+		
+		//Reverses the String
+		
+		StringBuilder sb = new StringBuilder("ABCD");
+		sb.reverse();
+		System.out.println(sb);
+
+	}
+
+}
