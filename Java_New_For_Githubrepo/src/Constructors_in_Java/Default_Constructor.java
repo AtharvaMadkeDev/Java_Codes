6b2af2d7e0313135b25aa902242f83aa319607fb
AtharvaMadkeDev <atharvamadke22@gmail.com>
2025-12-25 23:21:33 +0530

    Java Codes

--- Java_New_For_Githubrepo/src/Constructors_in_Java/Default_Constructor.java
+++ Java_New_For_Githubrepo/src/Constructors_in_Java/Default_Constructor.java
@@ -5,6 +5,8 @@ public class Default_Constructor {
 	public static void main(String[] args) {
 		
 		//When you dont manually writw a constructor, Java creates a constructor by default.
+		One_1 o = new One_1();
+		o.show();
 		
 
 	}
@@ -13,5 +15,11 @@ public class Default_Constructor {
 
 class One_1
 {
+	int id;
 	//An invisible constructor created by java by default
+	
+	public void show()
+	{
+		System.out.println("ID : "+id);
+	}
 }
---
+++ Java_New_For_Githubrepo/src/Constructors_in_Java/Non_Parameterized_Constructor.java
@@ -0,0 +1,29 @@
+package Constructors_in_Java;
+
+public class Non_Parameterized_Constructor {
+
+	public static void main(String[] args) {
+		
+		//It is a constructor which does not have any parameters.
+		One_2 o = new One_2();
+		o.show();
+		
+	}
+
+}
+
+//Syntax :
+
+class One_2
+{
+	int id;
+	public One_2()		//Parameter-less constructor
+	{
+		this.id=100;
+	}
+	
+	public void show()
+	{
+		System.out.println("ID : "+id);
+	}
+}
