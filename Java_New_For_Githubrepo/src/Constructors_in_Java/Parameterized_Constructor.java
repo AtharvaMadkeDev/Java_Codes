154317cc3f6949685b16ffaafa777f91910e82eb
AtharvaMadkeDev <atharvamadke22@gmail.com>
2025-12-25 23:29:37 +0530

    Java Codes

---
+++ Java_New_For_Githubrepo/src/Constructors_in_Java/Parameterized_Constructor.java
@@ -0,0 +1,31 @@
+package Constructors_in_Java;
+
+public class Parameterized_Constructor {
+
+	public static void main(String[] args) {
+		
+		//It is a constructor which takes/accepts parameters
+		
+		One_3 one = new One_3(23);		//A constructor call which accepts an id as a parameter
+		one.show();
+
+	}
+
+}
+
+//Syntax : 
+
+class One_3
+{
+	int id;
+	
+	public One_3(int id)	//Constructor taking parameters
+	{
+		this.id=id;
+	}
+	
+	public void show()
+	{
+		System.out.println("ID : "+id);
+	}
+}
