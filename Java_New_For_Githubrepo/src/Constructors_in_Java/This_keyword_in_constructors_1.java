2157a84330b0113842e29c14b1b2c37721cda2d0
AtharvaMadkeDev <atharvamadke22@gmail.com>
2026-01-12 11:42:41 +0530

    Java Codes

---
+++ Java_New_For_Githubrepo/src/Constructors_in_Java/This_keyword_in_constructors_1.java
@@ -0,0 +1,36 @@
+package Constructors_in_Java;
+
+public class This_keyword_in_constructors_1 {
+
+	public static void main(String[] args) {
+		
+		
+		//"this" refers to the current class object.
+		//It is used to initialize instance variables as well as to pass the value from the constructor to the class methods.
+		
+		//Syntax for setting up instance variables :
+		
+		This_keyword thiskeyword = new This_keyword(1,"Atharva");
+		thiskeyword.show();
+
+	}
+
+}
+
+class This_keyword
+{
+	int id;
+	String name;
+	
+	public This_keyword(int id, String name)
+	{
+		this.id=id;
+		this.name=name;
+	}
+	
+	public void show()
+	{
+		System.out.println("ID : "+id);
+		System.out.println("Name : "+name);
+	}
+}
